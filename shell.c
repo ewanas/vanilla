29c2ea1e3774f6e96d60058179160fe62f80fa6d
EL-Hassan Wanas <elhassan.wanas@gmail.com>
2014-10-28 05:56:32 +0300

    Okay, going to be a little more crazy with this

--- shell.c
+++ shell.c
@@ -16,46 +16,55 @@ void shl_exit(int);
 #define QUIT          0
 
 typedef struct cmd {
-  char*       command;
-  struct cmd* pipe;
+  // The command name
+  char*       command; 
+
+  // A pointer to another command to which output is piped
+  // Or NULL if none exist
+  struct cmd* pipe;     
+  
+  // The options to the command, including the command itself
   char*       options[MAX_OPT_COUNT + 1];
 } Cmd;
 
+// Used whenever there is an error during malloc
 #define MEMERROR(reason) { printf("Memory error: %s\n", reason); shl_exit(FAILURE); }
 
 /*
  * Parses the given string into a linked list showing each command linked
  * to the command that it pipes to
  */
-Cmd* getCmds(char* rawcmd) {
+Cmd* shl_parse_cmd(char* rawcmd) {
   char* buffer;
-  Cmd* firstCmd = NULL;
-  Cmd* lastCmd = NULL;
+  Cmd* first_cmd = NULL;
+  Cmd* last_cmd = NULL;
 
   buffer = strtok(rawcmd, PIPE);
 
+  // Parse everything in between PIPE's
   if(buffer != NULL) {
     do {
-      Cmd* newCmd = malloc(sizeof(Cmd));
-      if(newCmd == NULL) MEMERROR(rawcmd);
+      Cmd* new_cmd = malloc(sizeof(Cmd));
+      if(new_cmd == NULL) MEMERROR(rawcmd);
 
-      if(firstCmd == NULL) {
-        firstCmd = newCmd;
-        firstCmd->command = buffer;
-        firstCmd->pipe = NULL;
-        lastCmd = firstCmd;
-      } else {
-        lastCmd->pipe = newCmd;
-        lastCmd = lastCmd->pipe;
-        lastCmd->command = buffer;
-        lastCmd->pipe = NULL;
+      if(first_cmd == NULL) { // Create the first element of the list
+        first_cmd = new_cmd;
+        first_cmd->command = buffer;
+        first_cmd->pipe = NULL;
+        last_cmd = first_cmd;
+      } else {                // Append a command to the command list
+        last_cmd->pipe = new_cmd;
+        last_cmd = last_cmd->pipe;
+        last_cmd->command = buffer;
+        last_cmd->pipe = NULL;
       }
     } while((buffer = (char*)strtok(NULL, PIPE)) != NULL);
   }
 
   // Explode the program options and place them in the right place
+  // Place the program name as the first option
   Cmd* c;
-  for(c = firstCmd; c != NULL; c = c->pipe){
+  for(c = first_cmd; c != NULL; c = c->pipe){
     c->command = strtok(c->command, " ");
     c->options[0] = c->command;
 
@@ -66,13 +75,13 @@ Cmd* getCmds(char* rawcmd) {
     }
   }
 
-  return firstCmd;
+  return first_cmd;
 }
 
 /*
  * Frees the resources used by the command list
  */
-void freeCmds(Cmd* c) {
+void shl_free_cmd(Cmd* c) {
   Cmd* nextCmd;
 
   if(c != NULL) {
@@ -87,41 +96,37 @@ void freeCmds(Cmd* c) {
 /*
  * Runs a given list of commands while setting up the appropriate pipes
  */
-int shl_exec(Cmd* cmdLine) {
+int shl_exec(Cmd* cmd_line) {
   int pid;
   int pipefds[2];
 
-  if (cmdLine != NULL && cmdLine->pipe == NULL) {
-    if(pid = fork()) {
-      exit(wait());
-    } else {
-      if(execvp(cmdLine->command, cmdLine->options)) {
-        perror(cmdLine->command);
+  if (cmd_line != NULL && cmd_line->pipe == NULL) { // A program without piping
+    if(execvp(cmd_line->command, cmd_line->options)) {
+      perror(cmd_line->command);
+      exit(errno);
+    }
+  } else if (cmd_line != NULL) {  // A program piped to another
+    pipe(pipefds);
+
+    if(pid = fork()) {  // First program
+      close(1);
+      close(pipefds[0]);
+      dup(pipefds[1]);
+
+      if(execvp(cmd_line->command, cmd_line->options)) {
+        perror(cmd_line->command);
         exit(errno);
       }
+    } else {  // Whatever else exists after the first program that is piped to
+      close(0);
+      dup(pipefds[0]);
+      close(pipefds[1]);
+
+      if(!fork()) shl_exec(cmd_line->pipe);
     }
-  } else if (cmdLine != NULL) {
-    pipe(pipefds);
-    if(pid = fork()) {
-      exit(wait());
-    } else {
-      if(pid = fork()) {
-        close(1); dup(pipefds[1]);
-        close(pipefds[0]);
-        if(execvp(cmdLine->command, cmdLine->options)) {
-          perror(cmdLine->command);
-          exit(errno);
-        }
-      } else {
-        close(0); dup(pipefds[0]);
-        close(pipefds[1]);
-        if(fork()) wait();
-        else shl_exec(cmdLine->pipe);
-      }
-    }
-  } else {
-    exit(0);
   }
+
+  exit(wait());
 }
 
 /*
@@ -130,7 +135,6 @@ int shl_exec(Cmd* cmdLine) {
 void shl_exit(int reason){
   switch(reason){
     case FAILURE:
-      printf("\nNo more input...\n");
       break;
     case QUIT:
       printf("\nQuitting...\n");
@@ -174,9 +178,11 @@ char* shl_prompt(){
  */
 int main(int argc, char** argv){
   while(1){
-    Cmd* cmdLine = getCmds(shl_prompt());
+    Cmd* cmd_line = shl_parse_cmd(shl_prompt());
+
     if(fork()) wait();
-    else shl_exec(cmdLine);
-    freeCmds(cmdLine);
+    else shl_exec(cmd_line);
+
+    shl_free_cmd(cmd_line);
   }
 }
